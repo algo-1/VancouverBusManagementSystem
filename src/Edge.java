6861512b6761720843bd6be8b2d267e3481fe44f
Harold Karibiye <haroldkaribiye@gmail.com>
2022-04-04 14:50:32 +0100

    add another constructor to enable searching for arrival Time and sorting by trip_id

--- src/Edge.java
+++ src/Edge.java
@@ -1,11 +1,26 @@
-public class Edge {
+public class Edge implements Comparable<Edge>{
     int from;
     int to;
     double cost;
+    String arrivalTime = "";
+    Integer tripID = -1;
     Edge(int from, int to, double cost)
     {
         this.from = from;
         this.to = to;
         this.cost = cost;
+    }
+    Edge(int from, int to, double cost, String arrivalTime, int tripID)
+    {
+        this.from = from;
+        this.to = to;
+        this.cost = cost;
+        this.arrivalTime = arrivalTime;
+        this.tripID = tripID;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return this.tripID.compareTo(other.tripID);
     }
 }
