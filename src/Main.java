96b594ff73afe764c9ab4d41885f5b19f1c36e15
Harold Karibiye <haroldkaribiye@gmail.com>
2022-04-04 14:54:57 +0100

    add code for testing the arrival time search and sorting gy trip_id (Question 3)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
 import java.util.Scanner;
@@ -15,25 +16,35 @@ public class Main {
         Graph graph = new Graph(edges);
 
         // Q1
-        do {
-            System.out.print("Enter a start stop_id: ");
-            if (!in.hasNextInt()) break;
-            int sourceStopID = in.nextInt();
-            System.out.print("Enter a stop stop_id: ");
-            if (!in.hasNextInt()) break;
-            int endStopID = in.nextInt();
-            System.out.printf("start stop_id = %d stop stop_id = %d\n", sourceStopID, endStopID);
-            Deque<Pair<Integer, Double>> shortestPath = Dijkstra.dijkstra(sourceStopID, endStopID, stopIDs, graph);
-            for (Pair<Integer, Double> pair : shortestPath) {
-                System.out.printf("stop_id = %d cost = %f\n", pair.first, pair.second);
-            }
-        } while (true);
+//        do {
+//            System.out.print("Enter a start stop_id: ");
+//            if (!in.hasNextInt()) break;
+//            int sourceStopID = in.nextInt();
+//            System.out.print("Enter a stop stop_id: ");
+//            if (!in.hasNextInt()) break;
+//            int endStopID = in.nextInt();
+//            System.out.printf("start stop_id = %d stop stop_id = %d\n", sourceStopID, endStopID);
+//            Deque<Pair<Integer, Double>> shortestPath = Dijkstra.dijkstra(sourceStopID, endStopID, stopIDs, graph);
+//            for (Pair<Integer, Double> pair : shortestPath) {
+//                System.out.printf("stop_id = %d cost = %f\n", pair.first, pair.second);
+//            }
+//        } while (true);
 
 
         // Q2
 
 
         // Q3
+        do {
+            System.out.print("Enter an arrival time in format hh:mm:ss: ");
+            String arrivalTime = in.next();
+            if (arrivalTime.equals("quit")) break;
+            List<Edge> result = edges.stream()
+                    .filter(edge -> edge.arrivalTime.equals(arrivalTime)).sorted().collect(Collectors.toList());
+            for (Edge edge : result) {
+                System.out.printf("trip_id = %d, arrival time = %s\n", edge.tripID, edge.arrivalTime);
+            }
+        } while (true);
 
     }
 }
