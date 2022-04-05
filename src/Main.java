f91069c300bfe4f236a5d2705947661e25cc6610
Harold Karibiye <haroldkaribiye@gmail.com>
2022-04-05 23:55:35 +0100

    implement selection

--- src/Main.java
+++ src/Main.java
@@ -19,63 +19,83 @@ public class Main {
             tst.insert(stop.stopName, stop);
         }
 
-        // Q1
+        // CLI 
+        int select = 0;
         do {
-            System.out.print("Enter a start stop_id: ");
-            if (!in.hasNextInt()) break;
-            int sourceStopID = in.nextInt();
-            System.out.print("Enter a stop stop_id: ");
-            if (!in.hasNextInt()) break;
-            int endStopID = in.nextInt();
-            System.out.printf("start stop_id = %d stop stop_id = %d\n", sourceStopID, endStopID);
-            Deque<Pair<Integer, Double>> shortestPath = Dijkstra.dijkstra(sourceStopID, endStopID, stopIDs, graph);
-            for (Pair<Integer, Double> pair : shortestPath) {
-                System.out.printf("stop_id = %d cost = %f\n", pair.first, pair.second);
-            }
-        } while (true);
-
-
-        // Q2
-        in = new Scanner(System.in);
-        boolean done = false;
-        do {
-            String busStop = "";
-            do
+            System.out.print("Hello!\nEnter 1, 2, or 3 to select between shortest paths, " +
+                    "bus stop search or arrival time search respectively.\nEnter a number: ");
+            if (in.hasNextInt())
             {
-                System.out.print("Enter part of a bus stop name: ");
-                busStop = in.nextLine().trim().toUpperCase();
-                if (busStop.equalsIgnoreCase("quit")) done = true;
+                select = in.nextInt();
+            } else in.next();
+        } while (select < 1 || select > 3);
 
-            } while (busStop.length() == 0);
-
-            if (!busStop.equalsIgnoreCase("quit"))
-            {
-                List<Stop> matches = tst.find(busStop);
-                for (Stop stop : matches)
-                {
-                    if (stop == null) System.out.println();
-                    else System.out.printf("stop name = %s, stop id = %d\n", stop.stopName, stop.stopID);
+        if (select == 1)
+        {
+            // Q1
+            in = new Scanner(System.in);
+            do {
+                System.out.print("Enter a start stop_id: ");
+                if (!in.hasNextInt()) break;
+                int sourceStopID = in.nextInt();
+                System.out.print("Enter a stop stop_id: ");
+                if (!in.hasNextInt()) break;
+                int endStopID = in.nextInt();
+                System.out.printf("start stop_id = %d stop stop_id = %d\n", sourceStopID, endStopID);
+                Deque<Pair<Integer, Double>> shortestPath = Dijkstra.dijkstra(sourceStopID, endStopID, stopIDs, graph);
+                for (Pair<Integer, Double> pair : shortestPath) {
+                    System.out.printf("stop_id = %d cost = %f\n", pair.first, pair.second);
                 }
-            }
+            } while (true);
+        }
+        else if (select == 2)
+        {
+            // Q2
+            in = new Scanner(System.in);
+            boolean done = false;
+            do {
+                String busStop = "";
+                do
+                {
+                    System.out.print("Enter part of a bus stop name: ");
+                    busStop = in.nextLine().trim().toUpperCase();
+                    if (busStop.equalsIgnoreCase("quit")) done = true;
 
-        } while (!done);
+                } while (busStop.length() == 0);
 
-        // Q3
-        in = new Scanner(System.in);
-        do {
-            System.out.print("Enter an arrival time in format hh:mm:ss: ");
-            String arrivalTime = in.next();
-            if (arrivalTime.equals("quit")) break;
+                if (!busStop.equalsIgnoreCase("quit"))
+                {
+                    List<Stop> matches = tst.find(busStop);
+                    if (matches == null) System.out.println("no matches found.");
+                    else
+                    {
+                        for (Stop stop : matches)
+                        {
+                            System.out.printf("stop name = %s, stop id = %d\n", stop.stopName, stop.stopID);
+                        }
+                    }
 
-            List<Edge> result = edges.stream()
-                    .filter(edge -> Utils.isEqual(arrivalTime, edge.arrivalTime))
-                    .sorted()
-                    .collect(Collectors.toList());
+                }
 
-            for (Edge edge : result) {
-                System.out.printf("trip_id = %d, stop_id = %d, arrival time = %s\n", edge.tripID, edge.to, edge.arrivalTime);
-            }
-        } while (true);
+            } while (!done);
+        } else {
+            // Q3
+            in = new Scanner(System.in);
+            do {
+                System.out.print("Enter an arrival time in format hh:mm:ss: ");
+                String arrivalTime = in.next();
+                if (arrivalTime.equals("quit")) break;
+
+                List<Edge> result = edges.stream()
+                        .filter(edge -> Utils.isEqual(arrivalTime, edge.arrivalTime))
+                        .sorted()
+                        .collect(Collectors.toList());
+
+                for (Edge edge : result) {
+                    System.out.printf("trip_id = %d, stop_id = %d, arrival time = %s\n", edge.tripID, edge.to, edge.arrivalTime);
+                }
+            } while (true);
+        }
 
     }
 }
